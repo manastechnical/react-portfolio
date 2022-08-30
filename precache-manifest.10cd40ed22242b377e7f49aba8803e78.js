5bebb68e2de34e7269c326790804b0c59f8c2e47
Manas <manasjagtap20@gmail.com>
2022-08-30 22:43:28 +0530

    Updates

---
+++ precache-manifest.10cd40ed22242b377e7f49aba8803e78.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec6982ba20ad98fddaa4",
+    "url": "/react-portfolio/static/css/main.a451aca7.chunk.css"
+  },
+  {
+    "revision": "ec6982ba20ad98fddaa4",
+    "url": "/react-portfolio/static/js/main.ec6982ba.chunk.js"
+  },
+  {
+    "revision": "ee0cd147a8528fdc03a2",
+    "url": "/react-portfolio/static/js/1.ee0cd147.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "8c6dfc29d70a9c1f67dc0eeec3d32cac",
+    "url": "/react-portfolio/static/media/brandBanner.8c6dfc29.jpg"
+  },
+  {
+    "revision": "57de32c9147f009a6cea1ff828d83964",
+    "url": "/react-portfolio/static/media/About.57de32c9.jpg"
+  },
+  {
+    "revision": "907b19f5f18705ec06ff958bf68362f8",
+    "url": "/react-portfolio/static/media/5-2.907b19f5.jpg"
+  },
+  {
+    "revision": "a932d39816b87c0dd23d4bbfd9296402",
+    "url": "/react-portfolio/static/media/5-3.a932d398.jpg"
+  },
+  {
+    "revision": "ab7b46941b54c9326caee0459fa6380e",
+    "url": "/react-portfolio/static/media/5-4.ab7b4694.jpg"
+  },
+  {
+    "revision": "954f8f664d0aaf92592694d560adb0fe",
+    "url": "/react-portfolio/static/media/5-6.954f8f66.jpg"
+  },
+  {
+    "revision": "2d4137e959e15c9f46fe9931e07d960b",
+    "url": "/react-portfolio/static/media/5-8.2d4137e9.jpg"
+  },
+  {
+    "revision": "9f5d52bb3221676d38d57fa2ada6f278",
+    "url": "/react-portfolio/static/media/5-9.9f5d52bb.jpg"
+  },
+  {
+    "revision": "f370fd2b90a04799e11515f4039d69df",
+    "url": "/react-portfolio/static/media/contactus.f370fd2b.jpg"
+  },
+  {
+    "revision": "affa16066a3a49ec21cf2d6541dfbb80",
+    "url": "/react-portfolio/index.html"
+  }
+];
